5bf191d68b2fa05894035544a4417d22e39be822
Patrick Hilker <patrickhilker@users.noreply.github.com>
2022-02-20 01:56:34 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,12 +1,18 @@
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
+from homeassistant.const import CONF_ACCESS_TOKEN
+
 DOMAIN = "tedee"
 
 class TedeeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
     
-    async def async_step_user(self, info):
+    VERSION = 1
+    
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=Tedee, data=user_input)
+        
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({vol.Required(CONF_ACCESS_TOKEN, default='no access token given'): cv.string})
+            step_id="user", data_schema=vol.Schema({vol.Required(CONF_ACCESS_TOKEN): cv.string})
         )
