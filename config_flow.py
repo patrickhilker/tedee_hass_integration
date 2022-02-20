31c12c4c97a20056b36b924a4267c6965dcce935
Patrick Hilker <patrickhilker@users.noreply.github.com>
2022-02-20 02:04:48 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,15 +1,23 @@
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
+from homeassistant import config_entries
 
 from homeassistant.const import CONF_ACCESS_TOKEN
 
 DOMAIN = "tedee"
 
-class TedeeConfigFlow(self, domain=DOMAIN):
+class TedeeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    
+    def __init__(self):
+        """Initialize."""
+        self._errors = {}
     
     async def async_step_user(self, user_input=None):
+        self._errors = {}
+        
         if user_input is not None:
             return self.async_create_entry(title=Tedee, data=user_input)
         
