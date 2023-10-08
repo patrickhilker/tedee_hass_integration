af74eb498c5ffacf9315380e403950405d0e1b18
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-10-08 16:26:54 +0200

    init tests

---
+++ tests/test_config_flow.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+from custom_components.tedee.const import DOMAIN
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResultType
+
+def test_test() -> None:
+    assert True
+
+async def test_show_config_form(hass: HomeAssistant) -> None:
+    """Test if initial configuration form is shown."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+
+async def test_form(hass: HomeAssistant) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] is None
